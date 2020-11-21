b642a6de1c32494a5c6b20e77cfb997d5ee1a56f
SORavninov19 <63718888+SORavninov19@users.noreply.github.com>
2020-11-21 17:03:57 +0200

    Add emperimental score placeholder

--- Game/JavaScript/script.js
+++ Game/JavaScript/script.js
@@ -1,9 +1,11 @@
 var currentPlayer = true
-var currentMatch = [[0, 0, 0],
-                    [0, 0, 0],
-                    [0, 0, 0]]
+//whos turn is it
+var currentMatch =
+  [[0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]]
 var matchStatus;
-//is the match won and who won it.
+//is the match won and who won it
 function winCheck() {
   if (currentMatch[0][0] == currentMatch[0][1] && currentMatch[0][0] == currentMatch[0][2] && currentMatch[0][0] == 1) {
     matchStatus = true;
@@ -21,56 +23,56 @@ function winCheck() {
   //Match won by first player filling all lower spaces
 
   if (currentMatch[0][0] == currentMatch[0][1] && currentMatch[0][0] == currentMatch[0][2] && currentMatch[0][0] == 2) {
-    matchStatus = true;
+    matchStatus = false;
   }//Match won by second player filling all upper spaces
 
   if (currentMatch[1][0] == currentMatch[1][1] && currentMatch[1][0] == currentMatch[1][2] && currentMatch[1][0] == 2) {
-    matchStatus = true;
+    matchStatus = false;
   }
   //Match won by second player filling all middle spaces
 
   if (currentMatch[2][0] == currentMatch[2][1] && currentMatch[2][0] == currentMatch[2][2] && currentMatch[2][0] == 2) {
-    matchStatus = true;
+    matchStatus = false;
   }
   //Match won by second player filling all lower spaces
   if (currentMatch[0][0] == currentMatch[1][0] && currentMatch[0][0] == currentMatch[2][0] && currentMatch[0][0] == 1) {
     matchStatus = true;
   }
   //Match won by first player filling all left spaces
-    if (currentMatch[0][1] == currentMatch[1][1] && currentMatch[0][1] == currentMatch[2][1] && currentMatch[0][1] == 1) {
+  if (currentMatch[0][1] == currentMatch[1][1] && currentMatch[0][1] == currentMatch[2][1] && currentMatch[0][1] == 1) {
     matchStatus = true;
   }
   //Match won by first player filling all middle vertical spaces
-      if (currentMatch[0][2] == currentMatch[1][2] && currentMatch[0][2] == currentMatch[2][2] && currentMatch[0][2] == 1) {
+  if (currentMatch[0][2] == currentMatch[1][2] && currentMatch[0][2] == currentMatch[2][2] && currentMatch[0][2] == 1) {
     matchStatus = true;
   }
   //Match won by first player filling all right spaces
   if (currentMatch[0][0] == currentMatch[1][0] && currentMatch[0][0] == currentMatch[2][0] && currentMatch[0][0] == 2) {
-    matchStatus = true;
+    matchStatus = false;
   }
   //Match won by second player filling all left spaces
-    if (currentMatch[0][1] == currentMatch[1][1] && currentMatch[0][1] == currentMatch[2][1] && currentMatch[0][1] == 2) {
-    matchStatus = true;
+  if (currentMatch[0][1] == currentMatch[1][1] && currentMatch[0][1] == currentMatch[2][1] && currentMatch[0][1] == 2) {
+    matchStatus = false;
   }
   //Match won by second player filling all middle vertical spaces
-      if (currentMatch[0][2] == currentMatch[1][2] && currentMatch[0][2] == currentMatch[2][2] && currentMatch[0][2] == 2) {
-    matchStatus = true;
+  if (currentMatch[0][2] == currentMatch[1][2] && currentMatch[0][2] == currentMatch[2][2] && currentMatch[0][2] == 2) {
+    matchStatus = false;
   }
   //Match won by second player filling all right spaces
-    if (currentMatch[0][0] == currentMatch[1][1] && currentMatch[0][0] == currentMatch[2][2] && currentMatch[0][0] == 1) {
+  if (currentMatch[0][0] == currentMatch[1][1] && currentMatch[0][0] == currentMatch[2][2] && currentMatch[0][0] == 1) {
     matchStatus = true;
   }
   //Match won by first player filling all left diagonal spaces
-      if (currentMatch[0][2] == currentMatch[1][1] && currentMatch[0][2] == currentMatch[2][0] && currentMatch[0][2] == 1) {
+  if (currentMatch[0][2] == currentMatch[1][1] && currentMatch[0][2] == currentMatch[2][0] && currentMatch[0][2] == 1) {
     matchStatus = true;
   }
   //Match won by first player filling all right diagonal spaces
-      if (currentMatch[0][0] == currentMatch[1][1] && currentMatch[0][0] == currentMatch[2][2] && currentMatch[0][0] == 2) {
-    matchStatus = true;
+  if (currentMatch[0][0] == currentMatch[1][1] && currentMatch[0][0] == currentMatch[2][2] && currentMatch[0][0] == 2) {
+    matchStatus = false;
   }
   //Match won by second player filling all left diagonal spaces
-      if (currentMatch[0][2] == currentMatch[1][1] && currentMatch[0][2] == currentMatch[2][0] && currentMatch[0][2] == 2) {
-    matchStatus = true;
+  if (currentMatch[0][2] == currentMatch[1][1] && currentMatch[0][2] == currentMatch[2][0] && currentMatch[0][2] == 2) {
+    matchStatus = false;
   }
   //Match won by second player filling all right diagonal spaces
 }
@@ -106,6 +108,31 @@ function play(buttonID) {
 
   }
   winCheck();
-  console.log(matchStatus)
-
+  restartGame()
 }
+function restartGame() {
+
+  let player1wins = 0;
+  let player2wins = 0;
+  let ties;
+  if (matchStatus == true) {
+    player1wins++;
+    document.getElementById("scoreP1").innerHTML = "Player 1 score: " + player1wins;
+    clearboard();
+  }
+  if (matchStatus == false) {
+    player2wins++;
+    document.getElementById("scoreP2").innerHTML = "Player 2 score: " + player2wins;
+    clearboard();
+  }
+  console.log(player2wins)
+}
+function clearboard() {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      currentMatch[i][j] = 0;
+    }
+  }
+  document.getElementsByTagName("img")[0].style.src = ""
+}
+
